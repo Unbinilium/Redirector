60c741e59c3b586582a0faae4b28411c09233683
Unbinilium <15633984+Unbinilium@users.noreply.github.com>
2021-09-11 18:33:07 +0800

    Update redirect.js

--- js/redirect.js
+++ js/redirect.js
@@ -5,14 +5,12 @@ function redirector(url = [], timeout = 1000) {
     let response = new Array(url.length).fill(false)
     url.forEach((v, i) => {
         promise.push(
-            fetch(v, { mode: 'no-cors', signal: controller.signal })
-                .then(() => {
-                    response[i] = true
-                    console.log('Connetion success: ', v)
-                })
-                .catch(() => {
-                    console.log('Connetion failed: ', v)
-                })
+            fetch(v, { mode: 'no-cors', signal: controller.signal }).then(() => {
+                response[i] = true
+                console.log('Connetion success: ', v)
+            }).catch(() => {
+                console.log('Connetion failed: ', v)
+            })
         )
     })
     Promise.all(promise).finally(() => {
