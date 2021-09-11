e357da86660ecc84719c1b33307417b02cee7c4a
Unbinilium <15633984+Unbinilium@users.noreply.github.com>
2021-09-11 18:09:10 +0800

    Fix app.js sw path

---
+++ README.md
@@ -0,0 +1,21 @@
+## Redirector
+
+A service worker based URL redirector, redirect to an accessible URL form a test list with timeout, load once and redirect eveneven you're offline.
+
+### Config
+
+The default config stored in `js/config`, the position upper the priority higher, timeout unit `ms`:
+
+```js
+var testURL = [    
+    'http://10.0.0.1',
+    'http://example.com',
+    'http://github.com',
+]
+
+var testTimeout = 1000
+```
+
+### Acknowledge
+
+- [MDN - Using Service Workers](https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers)
--- js/app.js
+++ js/app.js
@@ -5,29 +5,29 @@ function redirectorWrapper() {
 function loadRedirector() {
     let script = document.createElement('script')
     script.src = 'js/config.js'
-    script.setAttribute('onload', 'testConnectionWrapper();')
+    script.setAttribute('onload', 'redirectorWrapper();')
     document.head.appendChild(script)
 }
 
 function loadServiceworker() {
-    if (document.readyState === 'complete') {
-        if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('js/sw.js', { scope: '/' }).then((reg) => {
-                if (reg.installing) {
-                    console.log('Service worker: installing')
-                } else if (reg.waiting) {
-                    console.log('Service worker: installed')
-                } else if (reg.active) {
-                    console.log('Service worker: active')
-                }
-            }).catch((err) => {
-                console.log('Service worker: registration failed with ' + err)
-            })
-        } 
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('sw.js', { scope: '/' }).then((reg) => {
+            if (reg.installing) {
+                console.log('Service worker: installing')
+            } else if (reg.waiting) {
+                console.log('Service worker: installed')
+            } else if (reg.active) {
+                console.log('Service worker: active')
+            }
+        }).catch((err) => {
+            console.log('Service worker: registration failed with ' + err)
+        })
     }
 }
 
 document.onreadystatechange = async () => {
-    loadServiceworker()
-    loadRedirector()
+    if (document.readyState === 'complete') {
+        loadServiceworker()
+        loadRedirector()
+    }
 }
