622d89d0b7e4ab10c39cddfc03f308a234e986e8
Unbinilium <15633984+Unbinilium@users.noreply.github.com>
2021-09-11 16:01:48 +0800

    Initial commit

---
+++ config.js
@@ -0,0 +1,6 @@
+var testURL = [    
+    'http://10.0.0.1',
+    'http://example.com'
+]
+
+var testTimeout = 1000
---
+++ redirect.js
@@ -0,0 +1,31 @@
+function testConnection(url = [], timeout = 1000) {
+    const controller = new AbortController()
+    const id = setTimeout(() => controller.abort(), timeout)
+    let promise = []
+    let response = new Array(url.length).fill(false)
+    url.forEach((v, i) => {
+        promise.push(
+            fetch(v, {
+                mode: 'no-cors',
+                signal: controller.signal
+            })
+                .then(() => {
+                    response[i] = true
+                    console.log('Connetion success: ', v)
+                })
+                .catch(() => {
+                    console.log('Connetion failed: ', v)
+                })
+        )
+    })
+    Promise.all(promise)
+        .finally(() => {
+            for (let i = 0; i != response.length; ++i) {
+                if (response[i]) {
+                    console.log('Redirecting to: ', url[i])
+                    window.location.href = url[i]
+                    break
+                }
+            }
+        })
+}
