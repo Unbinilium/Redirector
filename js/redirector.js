7d327d31c1df289e5dc2eb8d311fbb9df1e1c57d
Unbinilium <15633984+Unbinilium@users.noreply.github.com>
2021-09-14 13:38:58 +0800

    Refactor redirector.js

--- js/redirector.js
+++ js/redirector.js
@@ -1,38 +1,45 @@
-function redirector(url = [], timeout = 1000, interval = 3000) {
-    const controller = new AbortController()
-    const id = setTimeout(() => controller.abort(), timeout)
-    let promise = new Array()
-    let response = new Array(url.length).fill(false)
-    let redirected = false
-    url.forEach((v, i) => {
-        promise.push(
-            fetch(v, { mode: 'no-cors', signal: controller.signal }).then(() => {
-                response[i] = true
-                console.log('Connetion success: ', v)
-            }).catch(() => {
-                console.log('Connetion failed: ', v)
-            })
-        )
-    })
-    Promise.all(promise).finally(() => {
-        const baseParm = window.location.search
-        for (let i = 0; i != response.length; ++i) {
-            if (response[i]) {
-                redirected = true
-                let targetURL = new URL(url[i])
-                if (baseParm.startsWith('?to=')) {
-                    targetURL = new URL(baseParm.substring(4), targetURL.href)
-                }
-                console.log('Redirecting to: ', targetURL.href)
-                window.location.href = targetURL.href
-                break
-            }
-        }
-    }).then(() => {
-        if (!redirected) {
-            setTimeout(() => {
-                redirector(url, timeout, interval)
-            }, interval)
-        }
-    })
+async function redirector(urls = [], timeout = 1000, interval = 3000) {
+    const baseParm = window.location.search
+    let promises = new Array()
+    const controller = new AbortController()
+    const id = setTimeout(() => { controller.abort() }, timeout)
+    urls.forEach((url, index) => {
+        promises.push(new Promise((resolve, reject) => {
+            fetch(url, {
+                mode: 'no-cors', signal: controller.signal
+            }).then(() => {
+                console.log('Connection success:', url)
+                resolve(url)
+            }).catch((error) => {
+                console.log('Connection failed:', url)
+                reject(error)
+            })
+        }))
+    })
+    const responses = await Promise.allSettled(promises)
+    const target = new Promise((resolve, reject) => {
+        let redirected = false
+        responses.forEach((response) => {
+            if (!redirected && response.status === 'fulfilled') {
+                redirected = true
+                resolve(response.value)
+            } else if(response.status === 'rejected') {
+                console.log(response.reason)
+            }
+        })
+        if (!redirected) { reject(new Error('retrying after interval '+ interval + 'ms')) }
+    })
+    target.then((url) => {
+        let targetURL = new URL(url)
+        if (baseParm.startsWith('?to=')) {
+            targetURL = new URL(baseParm.substring(4), url)
+        }
+        console.log('Redirecting to: ', targetURL.href)
+        window.location.href = targetURL.href
+    }).catch((error) => {
+        setTimeout(() => {
+            console.log(error)
+            redirector(urls, timeout, interval)
+        }, interval)
+    })
 }
